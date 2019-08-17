f8c64769319c7f52fa96aa35ec4b40a866e9c705
Mitch Webster <mitch83083@gmail.com>
2019-08-17 13:09:59 -0400

    created first draft of database seed files

---
+++ sql-seed/table-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO user (userName, userPassword)
+VALUE
+()
+
+
+INSERT INTO categories (category, itemId)
+VALUE 
+()
+
+INSERT INTO items (itemName, itemCondition, itemQuantity, itemDescription)
+VALUE
+()
