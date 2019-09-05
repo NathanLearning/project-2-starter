cba187c13ee70a04576fd8445157eac93af94a45
Jalin Wiley <jmt.wiley@gmail.com>
2019-09-05 00:36:36 -0400

    password pseudo coded to index js

--- index.js
+++ index.js
@@ -49,3 +49,55 @@ app
           \`-----\`
           Server Started on http://localhost:${PORT}`)
   })
+  // password authentication -------------------------------------------------------------------------
+  // $(document).ready(function () {
+  //   $('#password')
+  //     .keyup(function () {
+  //       var pswd = $(this).val()
+  //       if (pswd.length < :sunglasses: {
+  //         $('#length')
+  //           .removeClass('valid')
+  //           .addClass('invalid')
+  //       } else {
+  //         $('#length')
+  //           .removeClass('invalid')
+  //           .addClass('valid')
+  //       }
+  //       if (pswd.match(/[a-z]/)) {
+  //         $('#letter')
+  //           .removeClass('invalid')
+  //           .addClass('valid')
+  //       } else {
+  //         $('#letter')
+  //           .removeClass('valid')
+  //           .addClass('invalid')
+  //       }
+  //       // validate capital letter
+  //       if (pswd.match(/[A-Z]/)) {
+  //         $('#capital')
+  //           .removeClass('invalid')
+  //           .addClass('valid')
+  //       } else {
+  //         $('#capital')
+  //           .removeClass('valid')
+  //           .addClass('invalid')
+  //       }
+  //       // validate number
+  //       if (pswd.match(/\d/)) {
+  //         $('#number')
+  //           .removeClass('invalid')
+  //           .addClass('valid')
+  //       } else {
+  //         $('#number')
+  //           .removeClass('valid')
+  //           .addClass('invalid')
+  //       }
+  //     // keyup code here
+  //     })
+  //     .focus(function () {
+  //       $('#pswd_info').show()
+  //     })
+  //     .blur(function () {
+  //       $('#pswd_info').hide()
+  //     })
+  //  })
